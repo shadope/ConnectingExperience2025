e6f6287950431d0d5a85e08948ea2eb90b534307
Shadow Sincross <shadowanneruby@gmail.com>
2025-01-29 23:13:37 -0600

    added spin

--- connectingexperience/gameObjects/throwables/scripts/RigidBody.gd
+++ connectingexperience/gameObjects/throwables/scripts/RigidBody.gd
@@ -8,10 +8,16 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
+	var rotateVal =randf_range(0,1)
+	self.apply_impulse(Vector2(0,-5).rotated(rotateVal), Vector2(0,15))
 	pass
 
 
 func throw(direction:Vector2, strength:float) -> void:
 	print("thrwoing")
 	var force = direction.normalized() * strength
-	self.apply_central_impulse( force)
+	self.apply_central_impulse(force)
+	
+	var rotateVal =randf_range(0,1)
+	self.apply_impulse(Vector2(0,-5).rotated(rotateVal), Vector2(0,15))
+	#add drag and make the tip more dense
